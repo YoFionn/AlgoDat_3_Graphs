37c293e987bb67a24d6221d02fb563bf2bccab42
YoFionn <ericksoncooperation@gmail.com>
2020-06-25 09:46:11 +0200

    Bitte pushe jetzt

--- source/GraphTemplate.hpp
+++ source/GraphTemplate.hpp
@@ -17,27 +17,58 @@ public:
     // TODO: implement additional constructors
     // TODO: implement method for adding a node
     // TODO: implement method for removing a node
-    // TODO: implement Prim
+
+
+    void prim() {
+        for (int i = 0; i < nodes.size(); i++) {
+            nodes[i].key = 2147483647;
+            nodes[i].parent = NULL;
+        }
+        minPriorityQueue.key = 0;
+        MinHeap Q = minPriorityQueue*;
+        while {Q != NULL}
+    }
+
+
     // TODO: implement Bellman-Ford
+
+
     // TODO: implement printGraph function that generates a file written using the dot format
 };
 
 class Node {
-private:
-    /* data */
+private: 
     std::string label;
     std::map<Node, int> adjacentNodes; // the int is for the weight od the egde
     Node *parent;
-    int distance; // aka key
-
+    int key;
 public:
-    Node(/* args */);
+    Node() :
+        label{},
+        adjacentNodes{},
+        parent{ nullptr },
+        key{}{};
+
+    Node(Node const& copy_node) :
+        label{copy_node.label},
+        adjacentNodes{copy_node.adjacentNodes},
+        parent{copy_node.parent},
+        key{copy_node.key}{}
+
     ~Node();
-    
-    // TODO: implement additional constructors
-    // TODO: implement method for adding a connection
-    // TODO: implement method for removing a connection
-    // TODO: implement methods for manipulating the parent and distance
+
+    void add_connection(Node const& new_node){
+        adjacentNodes.insert({ new_node, new_node.key });
+    }
+    void remove_connection(Node node) {
+        adjacentNodes.erase(node);
+    }
+    void set_key(int new_key) {
+        key = new_key;
+    }
+    void set_parent(Node* new_parent) {
+        parent = new_parent;
+    }
 };
 
 class MinHeap {
@@ -47,21 +78,15 @@ private:
 public:
     MinHeap(/* args */);
     ~MinHeap();
+
+
     // TODO: implement method for restructuring the min-priority Queue
     // TODO: implement method for extracting the smaller element from the min-priority Queue
 };
 
-class MinHeapNode {
-private:
-    /* data */
+struct MinHeapNode {    //were using a struct becouse no constructors are nassasary
     Node* node;
     MinHeapNode* parent;
     MinHeapNode* left;
     MinHeapNode* right;
-
-public:
-    MinHeapNode(/* args */);
-    ~MinHeapNode();
-
-    // TODO: implement additional constructors
 };
