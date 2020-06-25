bfd35905389051303bef8c7f471fcd4c6dd7bc21
Mielein <marie.trojan@uni-weimar.de>
2020-06-25 13:48:22 +0200

    save me

--- source/Graph.cpp
+++ source/Graph.cpp
@@ -1,5 +1,99 @@
 #define CATCH_CONFIG_RUNNER
+#include <bits/stdc++.h> 
 #include "GraphTemplate.hpp"
+
+Node Graph::addNode(Node node, std::iterator<int> pos){
+	nodes.insert(pos, node);
+}
+Node Graph::remove(std::iterator<int> pos){
+	nodes.erase(pos);
+}
+
+std::vector<Node> priorityQueue(std::vector<Node> nodeVec){
+	std::vector<Node> tmp;
+    std::priority_queue<Node> nodeQueue;
+    for(int i=0; i<nodeVec.size(); i++){
+        nodeQueue.push(nodeVec[i]);
+    }
+	while (!nodeQueue.empty()){
+		Node tmpNode = nodeQueue.top();
+		nodeQueue.pop();
+		tmp.push_back(tmpNode);
+	}
+	
+	return tmp;
+}
+//function to heapify tree(needed for extract)
+void MinHeap::heapify(Node root, Node n, std::vector<Node>){
+	
+}
+//returns min node from heap(priority Queue) and deletes it
+Node Minheap::extractMin(std::vector<Node> prioQueue){
+	//root is min value
+	Node min = prioQueue[0];
+	//copy value of last element into root ->delete last
+	prioQueue[0] = prioQueue.back();
+	prioQueue.pop_back();
+
+	int left
+}
+
+void Graph::prim() {
+    for (int i = 0; i < nodes.size(); i++) {
+        nodes[i].key = 2147483647;
+        nodes[i].parent = NULL;
+    }
+	//first node key = 0;
+	std::vector<Node> Q = priorityQueue(nodes);
+    while(!Q.empty()){
+		nodes[i] = Q.
+	}
+}
+
+bool Graph::BellmanFord(Node source){
+    for(int i = 0; i<nodes.size(); i++ ){
+        nodes[i].key = 2147483640;
+        nodes[i].parent = NULL;
+    }
+	source.key = NULL;
+	for(int i=1; i=nodes.size()-1; i++){
+		for(auto it= nodes[i].adjacentNodes.begin();it!=nodes[i].adjacentNodes.end();){
+			if((nodes[i].key + nodes[i].)< nodes[j].key){
+				nodes[j].key = nodes[i].key + nodes[i][j];
+				nodes[j].parent = i;
+			}
+		}
+		for()
+	}
+	return false
+}
+
+Node::Node():
+    label{},
+    adjacentNodes{},
+    parent{ nullptr },
+    key{}{}
+
+Node::Node(Node const& copy_node) :
+    label{copy_node.label},
+    adjacentNodes{copy_node.adjacentNodes},
+    parent{copy_node.parent},
+    key{copy_node.key}{}
+
+Node::~Node(){}
+
+void Node::addConnection(Node const& new_node){
+    adjacentNodes.insert({ new_node, new_node.key });
+}
+void Node::remove_connection(Node node) {
+    adjacentNodes.erase(node);
+}
+void Node::set_key(int new_key) {
+    key = new_key;
+}
+void Node::set_parent(Node* new_parent) {
+    parent = new_parent;
+}
 
 int main(int argc, char* argv[])
 {
--- source/GraphTemplate.hpp
+++ source/GraphTemplate.hpp
@@ -1,67 +1,44 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <bits/stdc++.h> 
 
 // YOU CAN USE ONLY THE VECTORS, STRINGS AND MAPS FOR THIS ASSIGNMENT!
 
 class Graph {
 private:
     std::vector<Node> nodes;
-    MinHeap *minPriorityQueue;
     bool isDirected;
 
 public:
-    Graph(/* args */);
+    Graph();
     ~Graph();
 
-    // TODO: implement additional constructors
-    // TODO: implement method for adding a node
-    // TODO: implement method for removing a node
-    // TODO: implement Prim
-    // TODO: implement Bellman-Ford
-    // TODO: implement printGraph function that generates a file written using the dot format
+    Node addNode(Node node, std::iterator<int> pos);
+    Node remove(std::iterator<int> pos);
+    void prim();
+    void BellmanFord(int source);
+ 
 };
 
-class Node {
-private:
-    /* data */
+struct Node {
     std::string label;
     std::map<Node, int> adjacentNodes; // the int is for the weight od the egde
     Node *parent;
-    int distance; // aka key
+    int key; // aka key
 
-public:
-    Node(/* args */);
-    ~Node();
+    void addConnection(Node const& new_node);
+    void remove_connection(Node node);
+    void set_key(int new_key);
+    void set_parent(Node* new_parent);
+};
     
-    // TODO: implement additional constructors
-    // TODO: implement method for adding a connection
-    // TODO: implement method for removing a connection
-    // TODO: implement methods for manipulating the parent and distance
-};
+struct MinHeap{
+    Node* left{};
+    Node* right{};
+    Node* parent{};
 
-class MinHeap {
-private:
-    MinHeapNode* root;
-
-public:
-    MinHeap(/* args */);
-    ~MinHeap();
-    // TODO: implement method for restructuring the min-priority Queue
-    // TODO: implement method for extracting the smaller element from the min-priority Queue
-};
-
-class MinHeapNode {
-private:
-    /* data */
-    Node* node;
-    MinHeapNode* parent;
-    MinHeapNode* left;
-    MinHeapNode* right;
-
-public:
-    MinHeapNode(/* args */);
-    ~MinHeapNode();
-
-    // TODO: implement additional constructors
+    std::vector<Node> priorityQueue(std::vector<Node> nodeVec);
+    void heapify(Node root, Node n, std::vector<Node>);
+    Node extractMin(std::vector<Node> prioQueue);
 };
