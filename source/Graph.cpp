605b945ba1ea001502e0245d5ad93077fab7d9d9
Mielein <marie.trojan@uni-weimar.de>
2020-06-25 15:10:37 +0200

    only print left

--- source/Graph.cpp
+++ source/Graph.cpp
@@ -1,15 +1,16 @@
 #define CATCH_CONFIG_RUNNER
 #include <bits/stdc++.h> 
 #include "GraphTemplate.hpp"
-
-<<<<<<< HEAD
+//adds node to graph
 Node Graph::addNode(Node node, std::iterator<int> pos){
 	nodes.insert(pos, node);
 }
+//removes node from Graph
 Node Graph::remove(std::iterator<int> pos){
 	nodes.erase(pos);
 }
-
+//priority queue needed in prim algorithm 
+//works as min heap
 std::vector<Node> priorityQueue(std::vector<Node> nodeVec){
 	std::vector<Node> tmp;
     std::priority_queue<Node> nodeQueue;
@@ -17,79 +18,63 @@ std::vector<Node> priorityQueue(std::vector<Node> nodeVec){
         nodeQueue.push(nodeVec[i]);
     }
 	while (!nodeQueue.empty()){
-=======
-Node Graph::addNode(Node node, std::iterator<int> pos) {
-	nodes.insert(pos, node);
-}
-Node Graph::remove(std::iterator<int> pos) {
-	nodes.erase(pos);
-}
-
-std::vector<Node> priorityQueue(std::vector<Node> nodeVec) {
-	std::vector<Node> tmp;
-	std::priority_queue<Node> nodeQueue;
-	for (int i = 0; i < nodeVec.size(); i++) {
-		nodeQueue.push(nodeVec[i]);
-	}
-	while (!nodeQueue.empty()) {
->>>>>>> 24ea7fdc9f9fb08d37df5aaaa5cb3269c47db72b
 		Node tmpNode = nodeQueue.top();
 		nodeQueue.pop();
 		tmp.push_back(tmpNode);
 	}
-<<<<<<< HEAD
 	
 	return tmp;
 }
 //function to heapify tree(needed for extract)
-void MinHeap::heapify(Node root, Node n, std::vector<Node>){
-	
+void MinHeap::heapify(Node root, std::vector<Node> vecci){
+	Node max = root;
+	Node left = 2*root+1;
+	Node right = 2*root+2;
+	//left child larger than root
+	if(vecci[left] > vecci[max]){
+		max = left;
+	}
+	//if right child is larger than root
+	if(vecci[right] > vecci[max]){
+		max = right;
+	}
+	//if max is not root
+	if(max != root){
+		//swapping root and max
+		std::swap(vecci[root], vecci[max])
+		//recursive heapify subtree
+		heapify(max, vecci);
+	}
 }
 //returns min node from heap(priority Queue) and deletes it
 Node Minheap::extractMin(std::vector<Node> prioQueue){
-=======
-
-	return tmp;
-}
-//function to heapify tree(needed for extract)
-void MinHeap::heapify(Node root, Node n, std::vector<Node>) {
-
-}
-//returns min node from heap(priority Queue) and deletes it
-Node Minheap::extractMin(std::vector<Node> prioQueue) {
->>>>>>> 24ea7fdc9f9fb08d37df5aaaa5cb3269c47db72b
 	//root is min value
 	Node min = prioQueue[0];
 	//copy value of last element into root ->delete last
 	prioQueue[0] = prioQueue.back();
 	prioQueue.pop_back();
 
-	int left
+	heapify(min, prioQueue);
+	return min;
 }
 
 void Graph::prim() {
-<<<<<<< HEAD
     for (int i = 0; i < nodes.size(); i++) {
         nodes[i].key = 2147483647;
         nodes[i].parent = NULL;
     }
-	//first node key = 0;
+	key = 0;
 	std::vector<Node> Q = priorityQueue(nodes);
     while(!Q.empty()){
-=======
-	for (int i = 0; i < nodes.size(); i++) {
-		nodes[i].key = 2147483647;
-		nodes[i].parent = NULL;
-	}
-	//first node key = 0;
-	std::vector<Node> Q = priorityQueue(nodes);
-	while (!Q.empty()) {
->>>>>>> 24ea7fdc9f9fb08d37df5aaaa5cb3269c47db72b
-		nodes[i] = Q.
+		nodes[i] = extractMin(nodes);
+		for (int i = 0; i < nodes.size(); i++){
+			if(nodes[i].adjacentNodes && adjacentNodes[i] < nodes[i].adjacentNodes.key){
+				nodes[i].adjacentNodes.parent = nodes[i].parent;
+				nodes[i].adjacentNodes.key = nodes[i].key
+			}
 	}
 }
 
-<<<<<<< HEAD
 bool Graph::BellmanFord(Node source){
     for(int i = 0; i<nodes.size(); i++ ){
         nodes[i].key = 2147483640;
@@ -98,32 +83,19 @@ bool Graph::BellmanFord(Node source){
 	source.key = NULL;
 	for(int i=1; i=nodes.size()-1; i++){
 		for(auto it= nodes[i].adjacentNodes.begin();it!=nodes[i].adjacentNodes.end();){
-			if((nodes[i].key + nodes[i].)< nodes[j].key){
-=======
-bool Graph::BellmanFord(Node source) {
-	for (int i = 0; i < nodes.size(); i++) {
-		nodes[i].key = 2147483640;
-		nodes[i].parent = NULL;
-	}
-	source.key = NULL;
-	for (int i = 1; i = nodes.size() - 1; i++) {
-		for (auto it = nodes[i].adjacentNodes.begin(); it != nodes[i].adjacentNodes.end();) {
-			if ((nodes[i].key + nodes[i].) < nodes[j].key) {
->>>>>>> 24ea7fdc9f9fb08d37df5aaaa5cb3269c47db72b
-				nodes[j].key = nodes[i].key + nodes[i][j];
-				nodes[j].parent = i;
+			if((nodes[i].key + nodes[i].adjacentNodes) < nodes[i].adjacentNodes.key){
+				nodes[i].adjacentNodes.key = nodes[i].key + nodes[i].adjacentNodes;
+				nodes[i].parent = i;
 			}
 		}
-<<<<<<< HEAD
-		for()
-=======
-		for ()
->>>>>>> 24ea7fdc9f9fb08d37df5aaaa5cb3269c47db72b
+	for(auto it= nodes[i].adjacentNodes.begin();it!=nodes[i].adjacentNodes.end();){
+		if((nodes[i].key + nodes[i].adjacentNodes)<nodes[i].adjacentNodes.key){
+			return false
+		}
 	}
-	return false
+	return true
 }
 
-<<<<<<< HEAD
 Node::Node():
     label{},
     adjacentNodes{},
@@ -149,33 +121,6 @@ void Node::set_key(int new_key) {
 }
 void Node::set_parent(Node* new_parent) {
     parent = new_parent;
-=======
-Node::Node() :
-	label{},
-	adjacentNodes{},
-	parent{ nullptr },
-	key{}{}
-
-Node::Node(Node const& copy_node) :
-	label{ copy_node.label },
-	adjacentNodes{ copy_node.adjacentNodes },
-	parent{ copy_node.parent },
-	key{ copy_node.key }{}
-
-Node::~Node() {}
-
-void Node::addConnection(Node const& new_node) {
-	adjacentNodes.insert({ new_node, new_node.key });
-}
-void Node::remove_connection(Node node) {
-	adjacentNodes.erase(node);
-}
-void Node::set_key(int new_key) {
-	key = new_key;
-}
-void Node::set_parent(Node* new_parent) {
-	parent = new_parent;
->>>>>>> 24ea7fdc9f9fb08d37df5aaaa5cb3269c47db72b
 }
 
 int main(int argc, char* argv[])
--- source/GraphTemplate.hpp
+++ source/GraphTemplate.hpp
@@ -18,35 +18,21 @@ public:
     Node remove(std::iterator<int> pos);
     void prim();
     void BellmanFord(int source);
-<<<<<<< HEAD
- 
-=======
-
->>>>>>> 24ea7fdc9f9fb08d37df5aaaa5cb3269c47db72b
 };
 
 struct Node {
     std::string label;
-    std::map<Node, int> adjacentNodes; // the int is for the weight od the egde
-<<<<<<< HEAD
+    std::map<Node, int> adjacentNodes; // the int is for the weight of the egde
     Node *parent;
-=======
-    Node* parent;
->>>>>>> 24ea7fdc9f9fb08d37df5aaaa5cb3269c47db72b
-    int key; // aka key
+    int key;
 
     void addConnection(Node const& new_node);
     void remove_connection(Node node);
     void set_key(int new_key);
     void set_parent(Node* new_parent);
 };
-<<<<<<< HEAD
     
 struct MinHeap{
-=======
-
-struct MinHeap {
->>>>>>> 24ea7fdc9f9fb08d37df5aaaa5cb3269c47db72b
     Node* left{};
     Node* right{};
     Node* parent{};
